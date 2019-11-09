047b232bd18a802097e29f0238c445c137a269d5
Ganesh-Test <57554746+Ganesh-Test@users.noreply.github.com>
2019-11-09 11:59:17 +0530

    Add files via upload
    Uploaded two fles

---
+++ Axis.java
@@ -0,0 +1,17 @@
+package bank;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+ class Axis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "D:\\GANESH @ QT\\chrome driver\\chromedriver.exe");
+WebDriver d=new ChromeDriver();
+d.get("https://www.axisbank.com/bank-smart/internet-banking/getting-started");
+	
+
+	}
+
+}
